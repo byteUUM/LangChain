1bdb459051f53dd143fe70c85cb47b5e0a2e5fb6
byteUUM <xyk719@126.com>
2026-04-10 19:23:09 +0800

    执行链

---
+++ learn/deepseek-test.py
@@ -0,0 +1,52 @@
+
+# # 定义大模型
+# from langchain_openai import ChatOpenAI
+
+# model = ChatOpenAI(
+#     model="deepseek-chat",        # 或 deepseek-reasoner（R1推理模型）
+#     api_key="sk-3d4558aed3ea4143ba709345860d5a71",
+#     base_url="https://api.deepseek.com/v1",
+# )
+
+
+# # 定义消息列表
+# from langchain_core.messages import HumanMessage, SystemMessage
+
+# messages = [
+#     SystemMessage(content="你是一个翻译官，把用户输入的信息翻译成地道的英语"),
+#     HumanMessage(content="阴天你看不见我，天晴你会想起我！"),
+# ] 
+# # 调用大模型
+# result = model.invoke(messages)
+# print(result)
+
+# # 解析输出
+# from langchain_core.output_parsers import StrOutputParser
+# parser = StrOutputParser()
+# print(parser.invoke(result))
+
+# 定义大模型
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(
+    model="deepseek-chat",        # 或 deepseek-reasoner（R1推理模型）
+    api_key="sk-3d4558aed3ea4143ba709345860d5a71",
+    base_url="https://api.deepseek.com/v1",
+)
+
+# 定义消息列表
+from langchain_core.messages import HumanMessage, SystemMessage
+
+messages = [
+    SystemMessage(content="你是一个翻译官，把用户输入的信息翻译成地道的英语"),
+    HumanMessage(content="阴天你看不见我，天晴你会想起我！"),
+] 
+
+# 定解析输出
+from langchain_core.output_parsers import StrOutputParser
+parser = StrOutputParser()
+
+# 执行链
+chain = model | parser
+result = chain.invoke(messages)
+print(result)
