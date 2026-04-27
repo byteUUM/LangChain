e18d5cba95b468188b2c38bbd4a4d9bd0717a0fc
byteUUM <xyk719@126.com>
2026-04-27 23:05:59 +0800

    .

--- tool/tool_use.py
+++ tool/tool_use.py
@@ -28,22 +28,23 @@ tools = [add, multiply]
 model_with_tools = model.bind_tools(tools = tools)
 # 调用工具
 msg = model_with_tools.invoke("45乘2等于多少？") # 没有真正调用工具，只是匹配工具？
-print(multiply.invoke(msg.tool_calls[0]))
 
-# 定义消息列表
+print(model_with_tools.invoke("45乘2等于多少？"))
+print(multiply.invoke(msg.tool_calls[0]).content)
 
-message = [
-    HumanMessage("50加4等于多少? 7乘11等于多少?")
-]
-ai_msg = model_with_tools.invoke(message)
-message.append(ai_msg)
+# # 定义消息列表
+# message = [
+#     HumanMessage("50加4等于多少? 7乘11等于多少?")
+# ]
+# ai_msg = model_with_tools.invoke(message)
+# message.append(ai_msg)
 
-for tool_call in ai_msg.tool_calls:
-    selected_tool = {"add":add, "multiply":multiply}[tool_call["name"].lower()] #名称匹配tool忽略大小写
-    tool_msg = selected_tool.invoke(tool_call) #执行tool
-    message.append(tool_msg)
+# for tool_call in ai_msg.tool_calls:
+#     selected_tool = {"add":add, "multiply":multiply}[tool_call["name"].lower()] #名称匹配tool忽略大小写
+#     tool_msg = selected_tool.invoke(tool_call) #执行tool
+#     message.append(tool_msg)
     
-print(message)
-print(model.invoke(message).content)
-print(model_with_tools.invoke(message).content)
+# print(message)
+# print(model.invoke(message).content)
+# print(model_with_tools.invoke(message).content)
 
