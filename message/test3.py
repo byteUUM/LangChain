416c67fb908d34a7d60164ce3f314415091e017c
byteUUM <xyk719@126.com>
2026-05-19 20:51:11 +0800

    消息筛选与合并

--- message/test3.py
+++ message/test3.py
@@ -12,6 +12,7 @@ messages = [
 
 # 按照类型筛选
 print(filter_messages(include_types="human").invoke(messages))
+# 或
 print(filter_messages(messages, include_types="human"))
 
 # 按照id筛选
@@ -20,23 +21,24 @@ print(filter_messages(messages, exclude_ids=["3"]))
 # 按照id+类型筛选
 print(filter_messages(messages, exclude_ids=["3"], include_types=[HumanMessage, AIMessage]))
 
-# model = ChatOpenAI(model="deepseek-chat")
+model = ChatOpenAI(model="deepseek-chat")
 
-# # 历史消息记录
-# messages = [
-#     SystemMessage("你是一个聊天助手。"),
-#     SystemMessage("你总是以笑话回应。"),
-#     HumanMessage("为什么要使用 LangChain?"),
-#     HumanMessage("为什么要使用 LangGraph?"),
-#     AIMessage("因为当你试图让你的代码更有条理时，LangGraph 会让你感到“节点”是个好主意！"),
-#     AIMessage("不过别担心，它不会“分散”你的注意力！"),
-#     HumanMessage("选择LangChain还是LangGraph?"),
-# ]
+# 历史消息记录
+messages = [
+    SystemMessage("你是一个聊天助手。"),
+    SystemMessage("你总是以笑话回应。"),
+    HumanMessage("为什么要使用 LangChain?"),
+    HumanMessage("为什么要使用 LangGraph?"),
+    AIMessage("因为当你试图让你的代码更有条理时，LangGraph 会让你感到“节点”是个好主意！"),
+    AIMessage("不过别担心，它不会“分散”你的注意力！"),
+    HumanMessage("选择LangChain还是LangGraph?"),
+]
 
 # 方式1
-# merger_message = merge_message_runs(messages)
-# model.invoke(merger_message).pretty_print()
+merger_message = merge_message_runs(messages)
+model.invoke(merger_message).pretty_print()
 
 # 方式2
-# chain = merger | model
-# chain.invoke(messages).pretty_print()
+merger = merge_message_runs()
+chain = merger | model
+chain.invoke(messages).pretty_print()
